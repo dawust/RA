3c2e762c5f118e81ddc39df462ac9fda43a8b16c
Daniel Wust <spdawust@stud.informatik.uni-erlangen.de>
2015-11-02 22:04:09 +0100

    Fixed gensamples

--- ue2/gensamples.sh
+++ ue2/gensamples.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+function vec {
+        result=`./$1 $kb | sed 's/.*MUPS //'`
+	echo "$2 $result" >> $1_data.dat
+	echo "$1 $2 $result"
+}
+
 START=1
 STOP=$((128*1024))
 
-y=`echo "e^(l($STOP/$START)/64)" | bc -l`
+y=`echo "e(l($STOP/$START)/64)" | bc -l`
 
 for i in `seq 0 64`
 do
-	kb=`echo "$START*$y^$i" | bc -l | xargs printf "%d\n"`
-	echo $kb
-	echo $y
+	kb=`echo "$START*$y^$i" | bc -l | xargs printf "%d\n" 2> /dev/null`
+	
+	vec vecsum_u1 $kb 
+	vec vecsum_u2 $kb 
+	vec vecsum_u3 $kb 
+	vec vecsum_u4 $kb 
+	vec vecsum_u8 $kb 
+	vec vecsum_u1_uc $kb 
+	vec vecsum_u2_uc $kb 
+	vec vecsum_u3_uc $kb 
+	vec vecsum_u4_uc $kb 
+	vec vecsum_u8_uc $kb 
 done
+
