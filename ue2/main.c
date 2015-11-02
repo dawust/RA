d624b2409361c86dbfdf0742b2076822fb4a1765
Daniel Wust <rech037@lima1.rrze.uni-erlangen.de>
2015-11-02 21:57:52 +0100

    Implemented manual loop unrolling

--- ue2/main.c
+++ ue2/main.c
@@ -1,20 +1,25 @@
+#define _GNU_SOURCE
+#define VECVALUE 1.0
+
+#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
+#include <sched.h>
 #include "vecsum.h"
+
 
 void initvector(float *A, int N) {
 	for (int i = 0; i < N; ++i) {
-		A[i] = 1.0;
+		A[i] = VECVALUE;
 	}
 }
 
 int main(int argc, char **argv) {
-
 	if (argc != 2) {
 		printf("usage: %s vectorsize\n", argv[0]);
-		return -1;
+		exit(EXIT_FAILURE);
 	}
+
 	int size = atoi(argv[1]);
 	int elem = size * 1024 / sizeof(float);
 	
@@ -23,31 +28,43 @@ int main(int argc, char **argv) {
 	double timeend = 0.0;
 	double time = 0.0;
 
+	//Pin to specific CPU
+	cpu_set_t my_set;
+	CPU_ZERO(&my_set);
+	CPU_SET(0, &my_set);
+	if (sched_setaffinity(0, sizeof(cpu_set_t), &my_set) != 0) {
+		fprintf(stderr, "could not pin thread");
+		exit(EXIT_FAILURE);
+	}
+	
+	//Create aligned vector	
 	float *vector;
-
+	float res;
 	posix_memalign((void **)&vector, 64, elem * sizeof(float));
-
 	initvector(vector, elem);
 
+	//Measure execution time
 	int T = 0;	
-	for (T = 1; timeend - timestart < 1.0; T = T * 2) {
+	for (T = 1; timeend - timestart < 1.0; T *= 2) {
 
                 clock_gettime(CLOCK_MONOTONIC, &ts_time);
 		timestart = ts_time.tv_sec + ((double)ts_time.tv_nsec / 1E9);
 
 		for (int t = 0; t < T; ++t) {
-			vec_sum(vector, elem);
+			res = vec_sum(vector, elem);
 		}
 
                 clock_gettime(CLOCK_MONOTONIC, &ts_time);
 		timeend = ts_time.tv_sec + ((double)ts_time.tv_nsec / 1E9);
 
 		time = timeend - timestart; 
-		
-
 	}
 	T /= 2;
 
-	fprintf(stderr, "Time %f Iterations %d LUPS %f\n", time, T, (double)T / time);
+	fprintf(stdout, "Time %f Iterations %d MUPS %f\n", time, T, (double)((long)elem * T / 1000000) / time);
 	
+	//Result assertion 
+	if (res != VECVALUE * elem) {
+		fprintf(stderr, "Wrong result! %f %f\n", res, VECVALUE * elem);
+	}
 }
--- ue2/vecsum.c
+++ ue2/vecsum.c
@@ -1,9 +1,91 @@
 float vec_sum(float *A, int N) {
 	float tmp = 0.0;
+	
+#ifdef UNROLLCOMP
 
+	#pragma unroll(UNROLL)
 	for (int i = 0; i < N; ++i) {
 		tmp += A[i];
 	}
+
+#else
+
+	int iter = N / UNROLL;
+	int cnt = (iter * UNROLL);
+	int leftover = N % UNROLL;
+	
+	float tmp0 = 0.0;
+	float tmp1 = 0.0;
+	float tmp2 = 0.0;
+	float tmp3 = 0.0;
+	float tmp4 = 0.0;
+	float tmp5 = 0.0;
+	float tmp6 = 0.0;
+	float tmp7 = 0.0;
+
+	#pragma novector
+	#pragma nounroll
+	for (int i = 0; i < cnt; i += UNROLL) {
+		tmp0 += A[i];	
+	#if UNROLL > 1
+		tmp1 += A[i + 1];
+	#endif
+	#if UNROLL > 2
+		tmp2 += A[i + 2];
+	#endif
+	#if UNROLL > 3
+		tmp3 += A[i + 3];
+	#endif
+	#if UNROLL > 4
+		tmp4 += A[i + 4];
+	#endif
+	#if UNROLL > 5
+		tmp5 += A[i + 5];
+	#endif
+	#if UNROLL > 6
+		tmp6 += A[i + 6];
+	#endif
+	#if UNROLL > 7
+		tmp7 += A[i + 7];
+	#endif
+	}
+
+
+	switch (leftover) {
+		case 7: tmp6 += A[cnt + 6];
+		case 6: tmp5 += A[cnt + 5];
+		case 5: tmp4 += A[cnt + 4];
+		case 4: tmp3 += A[cnt + 3];
+		case 3: tmp2 += A[cnt + 2];
+		case 2: tmp1 += A[cnt + 1];
+		case 1: tmp0 += A[cnt];
+		case 0: ;
+	}
+
+	tmp = tmp0;
+	#if UNROLL > 1
+	tmp += tmp1;
+	#endif
+	#if UNROLL > 2
+	tmp += tmp2;
+	#endif
+	#if UNROLL > 3
+	tmp += tmp3;
+	#endif
+	#if UNROLL > 4
+	tmp += tmp4;
+	#endif
+	#if UNROLL > 5
+	tmp += tmp5;
+	#endif
+	#if UNROLL > 6
+	tmp += tmp6;
+	#endif
+	#if UNROLL > 7
+	tmp += tmp7;
+	#endif
+
+#endif
 
 	return tmp;
 }
